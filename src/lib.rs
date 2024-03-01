07dd5e51e6698a5ad4044748a4fbcc7ea8e55fb3
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2024-03-02 01:29:10 +0700

    Added: Test for multiple json deserialization

--- src/lib.rs
+++ src/lib.rs
@@ -15,4 +15,19 @@ mod tests {
 
         assert_eq!(expected_struct.beacons, data_struct.unwrap().beacons);
     }
+    
+    #[test]
+    fn json_deserialize_multiple_test() {
+        let input_string = "{\"beacons\":[{\"macAddress\":\"34:ab:95:73:5a:9a\",\"rssi\":-67},{\"macAddress\":\"7c:87:ce:49:2b:82\",\"rssi\":-42}]}";
+        
+        let data_struct: Result<BeaconList, serde_json::Error> = serde_json::from_str(&input_string);
+
+        let beacon_1 = Beacon { mac_address: "34:ab:95:73:5a:9a".into(), rssi: -67 };
+        let beacon_2 = Beacon { mac_address: "7c:87:ce:49:2b:82".into(), rssi: -42 };
+        let expected_struct = BeaconList {
+            beacons: vec![beacon_1, beacon_2],
+        };
+
+        assert_eq!(expected_struct.beacons, data_struct.unwrap().beacons)
+    }
 }
